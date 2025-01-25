7c928949f099c7638d38a96644f188834dc4a13d
Srijitpal98 <srijitpal1998@gmail.com>
2025-01-25 22:03:47 +0530

    BMS First API and DB connection

--- src/main/java/dev/srijit/BookMyShow/model/Seat.java
+++ src/main/java/dev/srijit/BookMyShow/model/Seat.java
@@ -2,6 +2,7 @@ package dev.srijit.BookMyShow.model;
 
 import dev.srijit.BookMyShow.model.constant.SeatStatus;
 import dev.srijit.BookMyShow.model.constant.SeatType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -10,9 +11,11 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
+@Entity(name = "seats")
 public class Seat extends BaseModel{
+    @Column(name = "seatrow")
     private int row;
+    @Column(name = "seatcol")
     private int col;
     private String seatNumber;
     @Enumerated(EnumType.STRING)
@@ -20,3 +23,9 @@ public class Seat extends BaseModel{
     @Enumerated(EnumType.STRING)
     private SeatStatus status;
 }
+
+/*
+    SELECT * FROM SEAT WHERE ROW = ? AND COL = ? AND SEATNUMBER IS NOT ?
+
+    findSeatByRowAndColNotSeatNumber(int row, int col, String seatNumber);
+ */
